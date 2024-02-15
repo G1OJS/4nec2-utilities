db8836efb4a4730a71bd5c8c5988fad184d43430
G1OJS <140509227+G1OJS@users.noreply.github.com>
2024-02-15 16:03:15 +0000

    Create Harmonise Grounds.py

---
+++ scripts/Harmonise Grounds.py
@@ -0,0 +1,28 @@
+# This script loops through all .nec files under the path below (including those in subfolders)
+# and creates a .nec.new file containing the contents of the nec file
+# if a line starts with GN, it is ignored and replaced by the string specifid in newground below
+# remove the comments in the last two lines causes the script to additionally 
+# a) rename the each nec file to .nec.old and b) rename the .nec.new file to .nec
+# USE AT YOUR OWN RISK & BACK UP important files FIRST
+
+path=r"C:\Users\drala\OneDrive\Desktop\NEC\HF Antennas"
+
+newground="GN 2 0 0 0 11 0.01"
+
+for root, d_names,f_names in os.walk(path):
+
+		for f in f_names:
+			if(f.endswith(".nec")):       
+				fi = open(os.path.join(root,f), "r")
+				fo = open(os.path.join(root,f) + ".new", "w")
+				for line in fi:
+					print(line[0:2])
+					if(line[0:2]=="GN"):
+						fo.write(newground + "\n")
+					else:
+						fo.write(line)
+				fo.close()
+				fi.close()
+#				os.rename(os.path.join(root,f), os.path.join(root,f) + ".old")
+#				os.rename(os.path.join(root,f) + ".new", os.path.join(root,f))
+		
